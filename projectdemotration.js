81643955b26c4c4ac84d9ab495af9511fea72e0b
nobin <nobinissac52@gmail.com>
2026-07-30 21:18:22 +0530

    project

---
+++ projectdemotration.js
@@ -0,0 +1,14 @@
+function display(){
+    console.log("Hello wrold");
+}
+display();
+
+const display1=function(){
+console.log("welcome to java wrold");
+}
+display1();
+
+const add=(a,b)=>{
+    return a-b;
+}
+console.log(add(100,86));
